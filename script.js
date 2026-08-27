66b7c645f734a6afdded1c915392017b417bc523
Suelen Custódio <sulen.ct@gmail.com>
2026-08-27 11:19:38 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const html = document.documentElement
+
+// forma explicativa de como fazer
+ // if(html.classList.contains('light')) {
+ //   html.classList.remove('light')
+ // } else {
+ //   html.classList.add('light')
+ // }
+
+// forma resumida
+ html.classList.toggle('light')
+
+ // pegar a tag img
+ const img = document.querySelector("#profile img")
+
+ // substituir a imagem
+  if(html.classList.contains('light')) {
+    // se tiver light mode, add a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+ 
+}
